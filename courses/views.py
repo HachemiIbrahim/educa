d69254b75b30a0bc70efa442ade1bf22d897b515
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2025-04-18 23:26:56 +0100

    reordering content

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,4 @@
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from django.apps import apps
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.forms.models import modelform_factory
@@ -5,7 +6,6 @@ from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
-
 from django.views.generic.list import ListView
 
 from .forms import ModuleFormSet
@@ -140,17 +140,17 @@ class ModuleContentListView(TemplateResponseMixin, View):
         return self.render_to_response({"module": module})
 
 
-# class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
-#     def post(self, request):
-#         for id, order in self.request_json.items():
-#             Module.objects.filter(id=id, course__owner=request.user).update(order=order)
-#         return self.render_json_response({"saved": "OK"})
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id, course__owner=request.user).update(order=order)
+        return self.render_json_response({"saved": "OK"})
 
 
-# class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
-#     def post(self, request):
-#         for id, order in self.request_json.items():
-#             Content.objects.filter(id=id, module__course__owner=request.user).update(
-#                 order=order
-#             )
-#         return self.render_json_response({"saved": "OK"})
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id, module__course__owner=request.user).update(
+                order=order
+            )
+        return self.render_json_response({"saved": "OK"})
